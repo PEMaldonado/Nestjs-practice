d8d82a7311ea0189a1a42cd7fe8f82a2b9a30f8e
Pablo Maldonado <pablomaldonado2086@gmail.com>
2023-09-06 08:40:55 -0300

    starting project

---
+++ src/schemas/task.schema.ts
@@ -0,0 +1,22 @@
+import {Schema, Prop} from "@nestjs/mongoose"
+
+@Schema({
+    timestamps:true
+})
+
+export class Task {
+    @Prop({
+        unique:true,
+        required:true,
+        trim:true
+    })
+    title:string
+    @Prop({
+        trim:true
+    })
+    description:string
+    @Prop({
+        default:false
+    })
+    done:boolean
+} 
